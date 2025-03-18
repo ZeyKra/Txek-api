cdac986caa2a360fe4205e5cec903155fbbc0f4d
adryan <42096409+ZeyKra@users.noreply.github.com>
2025-03-18 22:55:15 +0100

    [FEAT] Match type matching database structure

--- app/types/match.ts
+++ app/types/match.ts
@@ -3,12 +3,15 @@ import type { Round } from "./round"
 export type MatchStatus = "created" | "in_progress" | "completed" | "cancelled"
 
 export interface Match {
-  id: string
-  players: string[] // IDs des utilisateurs
-  status: MatchStatus
-  createdAt: Date
-  completedAt?: Date
-  rounds: Round[]
+  id?: string,
+  completed_at?: Date,
+  created_at?: Date,
+  local_player: string[], // IDs des utilisateurs
+  local_player_count: number,
+  owner: string, // ID de l'utilisateur
+  player_count: number,
+  status: MatchStatus,
+  round_count: number,
   winner: string | null // ID de l'utilisateur gagnant
 }
 
